63d7d7e8f5a5d9f70862df25ab058350a14c6356
kalkausar <kalkausar@student.gunadarma.ac.id>
2019-04-24 00:06:35 +0700

    Hotel Activity

--- app/src/main/java/com/example/kalkausar/latihan/HotelActivity.java
+++ app/src/main/java/com/example/kalkausar/latihan/HotelActivity.java
@@ -1,27 +1,19 @@
 package com.example.kalkausar.latihan;
 
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class HotelActivity extends AppCompatActivity {
-
-    TextView Text_tile, Text_desc;
+    private ProgressBar progressHotel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hotel);
         setTitle("Hotel");
-        
-        Text_tile = findViewById(R.id.textview_title_Hotel);
-        Text_desc = findViewById(R.id.textview_desc_hotel);
-
-        Typeface customfont = Typeface.createFromAsset(getAssets(), "font/contb.ttf");
-        Text_tile.setTypeface(customfont);
-
-        Typeface customfont2 = Typeface.createFromAsset(getAssets(), "font/contm.ttf");
-        Text_desc.setTypeface(customfont2);
+        progressHotel = (ProgressBar) findViewById(R.id.progressBar_hotel);
+        progressHotel.setVisibility(View.INVISIBLE);
     }
 }
